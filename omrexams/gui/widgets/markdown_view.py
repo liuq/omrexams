586989cd003165f52f7d200365c26bdb73549f5b
Luca Di Gaspero <l.digaspero@uniud.it>
2022-09-29 08:49:49 +0200

    PDF works like a charm.

--- omrexams/gui/widgets/markdown_view.py
+++ omrexams/gui/widgets/markdown_view.py
@@ -1,7 +1,17 @@
 from PySide6.QtWebEngineWidgets import QWebEngineView
+from PySide6.QtWebEngineCore import QWebEngineSettings
+from PySide6 import QtCore
+
+PDF = 'file:///Users/digaspero/Documents/Development/omrexams/multipage.pdf'
 
 class MarkdownView(QWebEngineView):
     """
     Webview for markdown
     """
-    pass
+    def __init__(self, widget):
+        super().__init__(widget)
+        self.settings().setAttribute(
+            QWebEngineSettings.PluginsEnabled, True)
+        self.settings().setAttribute(
+            QWebEngineSettings.PdfViewerEnabled, True)
+        self.load(QtCore.QUrl.fromUserInput(PDF))
